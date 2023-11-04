fdb187f7dc27b9731af1849445362846fab6b452
Osman Aslancan <osmanaslancan24@gmail.com>
2023-11-04 23:53:17 +0300

    add supabase authentication

--- InternshipBackend/Program.cs
+++ InternshipBackend/Program.cs
@@ -1,13 +1,38 @@
 using InternshipBackend.Data;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+
+builder.Configuration.AddJsonFile("appsettings.machine.json", true);
 
 builder.Services.AddDbContext<InternshipDbContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("Supabase")));
+
+
+builder.Services.AddAuthentication(o =>
+    {
+        o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        o.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(o =>
+    {
+        o.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = "https://vzmyswxvnmseubtqgjpc.supabase.co/auth/v1",
+            ValidAudience = "authenticated",
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SupabaseSigningKey"]
+                                                                               ?? throw new ArgumentNullException(nameof(o.TokenValidationParameters.IssuerSigningKey)))),
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
