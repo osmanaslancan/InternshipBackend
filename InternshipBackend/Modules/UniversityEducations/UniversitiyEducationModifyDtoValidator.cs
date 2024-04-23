d87d020e72309f854659f579552feaeecc562c03
osmanaslancan <osmanaslancan@users.noreply.github.com>
2024-04-23 14:42:16 +0300

    check for end date more than start date

--- InternshipBackend/Modules/UniversityEducations/UniversitiyEducationModifyDtoValidator.cs
+++ InternshipBackend/Modules/UniversityEducations/UniversitiyEducationModifyDtoValidator.cs
@@ -10,6 +10,7 @@ public class UniversitiyEducationModifyDtoValidator : AbstractValidator<Universi
         RuleFor(x => x.Department).NotNull();
         RuleFor(x => x.EducationYear).NotNull().LessThanOrEqualTo(6);
         RuleFor(x => x.StartDate).NotNull();
+        RuleFor(x => x.EndDate).GreaterThan(x => x.StartDate);
         RuleFor(x => x.IsGraduated).NotNull();
         RuleFor(x => x.Description).MaximumLength(1000);
         RuleFor(x => x.GPA).InclusiveBetween(0, 4);
